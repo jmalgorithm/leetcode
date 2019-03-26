e9443d0635c6f394e0ecb5af5c4e9238a7d7d5d8
yeahyung <35248661+yeahyung@users.noreply.github.com>
2019-03-26 14:08:13 +0900

    Update 46예형빈.cpp

--- 2/46예형빈.cpp
+++ 2/46예형빈.cpp
@@ -9,3 +9,25 @@ public:
         return answer;
     }
 };
+
+
+
+class Solution {
+public:
+    void per(vector<int>& nums,int begin,vector<vector<int>>& result){
+        if(begin>=nums.size()){
+            result.push_back(nums);
+            return;
+        }
+        for(int i=begin;i<nums.size();i++){
+            swap(nums[begin],nums[i]);
+            per(nums,begin+1,result);
+            swap(nums[begin],nums[i]);
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> result;
+        per(nums,0,result);
+        return result;
+    }
+};
